4db981d3605439aa169f4ac4e1c4c7bb308996e3
Bhanuka Praneeth <bhanuka9519@gmail.com>
2019-11-20 21:58:08 +0530

    selmo wep app index.php file

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php 
+  session_start();
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SELMO</title>
+	<style type="text/css">
+		.body{
+				background-image: url(images/new22.jpg);
+				background-size: cover;
+			
+
+			}
+		.welcome{
+
+			font-size: 80px;
+			text-align: center;
+			color: white;
+			text-shadow: 0 1px 0 #ccc,
+               0 2px 0 #c9c9c9,
+               0 3px 0 #bbb,
+               0 4px 0 #b9b9b9,
+               0 5px 0 #aaa,
+               0 6px 1px rgba(0,0,0,.1),
+               0 0 5px rgba(0,0,0,.1),
+               0 1px 3px rgba(0,0,0,.3),
+               0 3px 5px rgba(0,0,0,.2),
+               0 5px 10px rgba(0,0,0,.25),
+               0 10px 10px rgba(0,0,0,.2),
+               0 20px 20px rgba(0,0,0,.15);
+		}
+		.welcome2{
+			font-size: 80px;
+			text-align: center;
+			color: #C3073F;
+			text-shadow: 0 1px 0 #ccc,
+               0 2px 0 #c9c9c9,
+               0 3px 0 #bbb,
+               0 4px 0 #b9b9b9,
+               0 5px 0 #aaa,
+               0 6px 1px rgba(0,0,0,.1),
+               0 0 5px rgba(0,0,0,.1),
+               0 1px 3px rgba(0,0,0,.3),
+               0 3px 5px rgba(0,0,0,.2),
+               0 5px 10px rgba(0,0,0,.25),
+               0 10px 10px rgba(0,0,0,.2),
+               0 20px 20px rgba(0,0,0,.15); 
+		}
+
+	</style>
+
+</head>
+<body class="body">
+	<?php include('header.php'); ?>
+
+
+		<p class="welcome">WELCOME TO THE SRI LANKA's </p><p class="welcome2">NO. 01 </p><p class="welcome">SALES AND MARKETING PLATFORM</p>
+
+
+</div>
+</body>
+</html>
