0805b7f45b48abdf593c83a46bcc103a2ea70dca
Bhanuka Praneeth <bhanuka9519@gmail.com>
2019-11-30 22:14:06 +0530

    about page

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php 
+  session_start();
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tech world</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<style type="text/css">
+		body{
+			background-image: url(images/9.jpg);
+			background-repeat: no-repeat;
+			background-size: cover;
+		}
+	</style>
+</head>
+<body>
+
+<?php include('header.php'); ?>
+
+<div class="about">
+	<p>
+			Welcome to the Sri Lanka's No. 01 sales and marketing site. Are you a Vendor? Looking for a better market place to sell your products? Then SELMO is for you. Are you a Customer? looking for products to buy with AAA quality and trustworthy then you are also at the right place. Sign Up as a Vendor or Customer and get the advantage of this amazing platform.
+	</p>>	
+</div>
+
+</body>
+</html>
