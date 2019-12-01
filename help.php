248fbf67f4adc6672deeaa30fb641e5b929bc684
Bhanuka Praneeth <bhanuka9519@gmail.com>
2019-12-01 22:15:45 +0530

    add help page

---
+++ help.php
@@ -0,0 +1,36 @@
+<?php 
+session_start();
+ ?>
+ <!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Help</title>
+	<style type="text/css">
+		body{
+			background-image: url(images/9.jpg);
+			background-repeat: no-repeat;
+			background-size: cover;
+		}
+	</style>
+</head>
+<body>
+	<?php include('header.php'); ?>
+	
+	<div class="help">
+		<ul>
+			<li>Welcome to the selmo -The Sri Lanka's No. 1 Sales and Marketing Platform.</li>
+			<li>If you are a member you can login via input your email and password.</li>
+			<li>Else you can sign in to our site as a vendor or customer.</li>
+			<li>If you are a customer you can view products, add them in to the cart and buy products.</li>
+			<li>If you are a vendor you can add products, view products and update products.</li>
+			<li>If there are any problem with passwords, Contatct admin via contact page.</li>
+
+		</ul>
+
+	</div>
+
+
+
+</body>
+</html>
