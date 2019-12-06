77ba88d1f95b5f315feb218f017ca118bc2c64d5
Bhanuka Praneeth <bhanuka9519@gmail.com>
2019-12-06 22:18:16 +0530

    contact.php page

---
+++ contact.php
@@ -0,0 +1,33 @@
+<?php 
+  session_start();
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tech world</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<style type="text/css">
+		body{
+			background-image: url(images/7.jpg);
+			background-repeat: no-repeat;
+			background-size: cover;
+		}
+	</style>
+</head>
+<body>
+
+<?php include('header.php'); ?>
+
+<div class="contact">
+	<ul>
+		<li>Email:		<pre>     selmo@gmail.com</pre></li>
+		<li>TP:			<pre>     011-XXX XXXX</pre> </li>
+		<li>Address:	<pre>     SELMO, Edmonton rd, Colombo 07.</pre></li>
+	</ul>
+</div>
+
+
+</body>
+</html>
