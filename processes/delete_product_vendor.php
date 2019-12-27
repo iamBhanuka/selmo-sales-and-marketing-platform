a3594ce5981ed9e056a7f045e5cd70bbbbd98eeb
Bhanuka Praneeth <bhanuka9519@gmail.com>
2019-12-27 22:43:52 +0530

    delete product vendor

---
+++ processes/delete_product_vendor.php
@@ -0,0 +1,12 @@
+<?php 
+    $pid = $_GET['pid'];
+    $connect = mysqli_connect("localhost","root","","project");
+    $sql = "DELETE FROM products WHERE pid='$pid'";
+	$query = mysqli_query($connect, $sql);
+				
+    if($query){
+        header('Location: ../view_products_vendor.php');
+    } else {
+    }
+
+?>
